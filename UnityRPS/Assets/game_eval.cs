bc67379af6b034a494f4252c74b589217032bad8
paperboat34 <122484978+paperboat34@users.noreply.github.com>
2023-01-12 16:27:53 -0800

    work in progress

--- UnityRPS/Assets/game_eval.cs
+++ UnityRPS/Assets/game_eval.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class game_eval : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public Text Result;
+   public Image AIChoice;
+   
+
+   public string[] Choices;
+   public Sprite Rock, Paper, Scissors;
+   
+
+   public void Play(string myChoice)
     {
-        
+        string randomChoice = Choices[Random.Range(0,Choices.Length)];
+
+        switch(randomChoice)
+            {
+                case "Rock":
+                    switch(myChoice)
+                        {
+                            case "Rock":
+                            Result.text = "Tie";
+                            break;
+
+                            case "Paper":
+                            Result.text = "Win";
+                            break;
+
+                            case "Scissors":
+                            Result.text = "Loss";
+                            break;
+
+                        }
+                    AIChoice.sprite = Rock;
+                    break;
+
+                case "Paper":
+                    switch(myChoice)
+                        {
+                            case "Rock":
+                            Result.text = "Loss";
+                            break;
+
+                            case "Paper":
+                            Result.text = "Tie";
+                            break;
+
+                            case "Scissors":
+                            Result.text = "Win";
+                            break;
+
+                        }
+                    AIChoice.sprite = Paper;
+                    break;
+                
+                case "Scissors":
+                    switch(myChoice)
+                        {
+                            case "Rock":
+                            Result.text = "Win";
+                            break;
+
+                            case "Paper":
+                            Result.text = "Loss";
+                            break;
+
+                            case "Scissors":
+                            Result.text = "Tie";
+                            break;
+
+                        }
+                    AIChoice.sprite = Scissors;
+                    break;
+
+            }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
